9eeebf29351b52b42f4a84d855769042b439c747
yudistiraakun2 <yudistira.generasi.robot@gmail.com>
2021-01-05 10:04:55 +0700

    memperbaiki fixed scrolling body

--- script.js
+++ script.js
@@ -2,15 +2,27 @@
 $(window).scroll(function() {
 	var wScroll = $(this).scrollTop();
 
+	// jumbotron
 	$('.jumbotron img').css({
 		'transform' : 'translate(0, '+ wScroll/6 +'%)'
 	});
+
 
 	$('.jumbotron h1').css({
 		'transform' : 'translate(0, '+ wScroll/2 +'%)'
 	});
 
 	$('.jumbotron p').css({
-		'transform' : 'translate(0, '+ wScroll/1.6 +'%)'
+		'transform' : 'translate(0, '+ wScroll/1.1 +'%)'
 	});
+
+	// sambutan bikin ulang besok
+	// if(wScroll > $('.sambutan').offset().top - 250) {
+	// 	$('.sambutan .img-thumbnail').each(function(i) {
+	// 		setTimeout(function() {
+	// 			$('.sambutan .img-thumbnail').eq(i).addClass('muncul');
+	// 		}, 300 * (i+1));
+	// 	});
+
+	// }
 });
